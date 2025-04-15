0908844c994f204ea85e1cc049776cdac55fe0cb
SOF_blur <astralis.softworks@gmail.com>
2025-04-15 04:32:54 -0700

    w

--- listenerTest.lua
+++
@@ -1,5 +0,0 @@
-local Keys = loadstring(game:HttpGet("https://raw.githubusercontent.com/AstralisLLC/RobloxScripts/refs/heads/main/keys.lua"))()
-
-Keys.CreateListener("Test", "Return", function()
-    print("Listener \'test\' triggered!")
-end)
